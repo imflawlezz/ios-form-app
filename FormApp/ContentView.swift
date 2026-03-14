763a207cb0e2bd6e2afce76322bf834dec4edd53
imflawlezz <egorart6@gmail.com>
2026-03-14 02:44:27 +0100

    Initial Commit

---
+++ FormApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FormApp
+//
+//  Created by Yahor Artsiomchyk on 14/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FormApp/FormAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormAppApp.swift
+//  FormApp
+//
+//  Created by Yahor Artsiomchyk on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FormAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
