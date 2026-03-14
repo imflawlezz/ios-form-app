50fdb5f66047cd7cb42f9a25cf9fdb439cc2ddcc
imflawlezz <egorart6@gmail.com>
2026-03-14 03:51:53 +0100

    refactor: remove ContentView and integrate NavigationStack with ContactFormView

--- FormApp/FormAppApp.swift
+++ FormApp/FormAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct FormAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContactFormView()
+            }
         }
     }
 }
